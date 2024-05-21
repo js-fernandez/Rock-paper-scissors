81655f2ebb94e58a934bc298de30a75b8d861db4
js-fernandez <fernandez.jseb@gmail.com>
2024-05-21 00:18:57 -0300

    Nuevo rps

--- js/js.js
+++ js/js.js
@@ -1,77 +1,101 @@
-// FUNCTION COMPUTER CHOICE START
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const send = document.getElementById('send');
+const buttons = document.getElementsByTagName('button');
+const decisionHumano = document.getElementById('decision-humano')
+const decisionMaquina = document.getElementById('decision-maquina')
+const result = document.getElementById('result');
+const humanScore = document.querySelector('.human-score');
+const computerScore = document.querySelector('.computer-score');
+let humanCount = 0;
+let computerCount = 0;
+let playerChoice = '';
+let computerChoice = ''
+humanScore.textContent = humanCount;
+computerScore.textContent = computerCount
+
 function getComputerChoice () {
-    let numberRandom = Math.floor(Math.random() * 3) + 1
-    if (numberRandom == 1) {
-         numberRandom = 'rock'
-    }
-    else if (numberRandom == 2) {
-         numberRandom = 'paper'
-    }
-    else {
-         numberRandom = 'scissors'
-    }
-    return numberRandom
-};
-// FUNCTION COMPUTER CHOICE END
-
-// FUNCTION HUMAN CHOICE START
-function getHumanChoice() {
-    let choice = prompt("What's your move? \nRock\nPaper\nScissors")
-    choice = choice.toLowerCase();
-    if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
-     alert('Introduce a valid valor');
-     getHumanChoice();
-    }
-    else {
-     return choice
-    }
-};
-// FUNCTION HUMAN CHOICE END
-
-// SCORE VARIABLE START
-let humanScore = 0;
-let computerScore = 0;
-// SCORE VARIABLE END
-
-// PLAY ROUND FUNCTION START
-function playRound () {
-     computerChoice = getComputerChoice();
-     humanChoice = getHumanChoice();
-     if (humanChoice == 'rock' && computerChoice == 'rock'||
-         humanChoice == 'paper' && computerChoice == 'paper'|| 
-         humanChoice == 'scissors' && computerChoice == 'scissors'
-     ) {
-     }
-     else if (humanChoice == 'rock' && computerChoice == 'scissors'||
-              humanChoice == 'paper' && computerChoice == 'rock'|| 
-              humanChoice == 'scissors' && computerChoice == 'paper'
-     ) {
-          humanScore += 1
-          console.log(`Humano:${humanChoice}\n Computadora:${computerChoice}\n Score humano ${humanScore}\n Score computadora ${computerScore}`)
-     }
-     else if (humanChoice == 'rock' && computerChoice == 'paper'||
-           humanChoice == 'paper' && computerChoice == 'scissors'|| 
-           humanChoice == 'scissors' && computerChoice == 'rock'
-     ) {
-           computerScore += 1
-          console.log(`Humano:${humanChoice}\n Computadora:${computerChoice}\n Score humano ${humanScore}\n Score computadora ${computerScore}`)
+     let decision = Math.floor(Math.random() * 3)
+     switch (decision) {
+          case 0: {
+               computerChoice = 'rock'
+          }
+          break;
+          case 1: {
+               computerChoice = 'paper'
+          }
+          break;
+          case 2: {
+               computerChoice = 'scissors'
+          }
+          break;
      }
 }
-// PLAY ROUND FUNCTION END
 
-function playGame() {
-     for(i = 0; i < 5; i++){
-          playRound()
+ function addEvent(x) {
+     x.addEventListener('click', () => {
+         if (x.classList.contains('selected')) {
+             x.classList.remove('selected'); 
+         } else {
+             for (let button of buttons) {
+               button.classList.remove('selected')
+             }
+             x.classList.add('selected'); 
+         }
+         playerChoice = x.textContent.toLowerCase();
      }
-     if (humanScore == computerScore) {
-          console.log('EMPATE')
+);
+}
+
+addEvent(rock);
+addEvent(paper);
+addEvent(scissors);
+
+function verifyWin(x, winner) {
+     if(x == 5) {
+          result.textContent = `${winner} won the game.`
      }
-     else if (humanScore > computerScore) {
-          console.log('Gana humano')
+}
+
+send.addEventListener('click', ()=>{
+     getComputerChoice();
+     if (playerChoice == '') {
+          computerChoice == ''
+          alert('Debes seleccionar una opcion.')
+     }
+     if (humanCount == 5 || computerCount == 5) {
+          alert('Game is finished')
      }
      else {
-          console.log('Gana computadora')
+          
+          setTimeout(() => {
+               decisionMaquina.style = 'visibility: visible';
+               decisionHumano.style = 'visibility: visible'
+               decisionHumano.textContent = playerChoice;
+               decisionMaquina.textContent = computerChoice;
+          }, 150);
+               if (playerChoice == computerChoice) {
+               result.textContent = 'Draw'
+               }
+               else if (playerChoice == 'rock' && computerChoice == 'scissors' ||
+                   playerChoice == 'paper' && computerChoice == 'rock'    ||
+                   playerChoice == 'scissors' && computerChoice == 'paper'){
+                    result.textContent = 'Human win'
+                    humanCount += 1;
+                    setTimeout(() => {
+                         humanScore.textContent = humanCount;
+                    }, 150);
+                    verifyWin(humanCount, 'Human');
+           }
+     
+           else {
+                    result.textContent = 'Computer win'
+                    computerCount += 1;
+                    setTimeout(() => {
+                       computerScore.textContent = computerCount;  
+                    }, 150);
+               verifyWin(computerCount, 'Computer');
+               }
      }
-}
-
-playGame()
+})
